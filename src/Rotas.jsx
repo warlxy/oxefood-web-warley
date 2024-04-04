4ef76f1bad2b7cb152864c385721a0f354725e7d
aluno <>
2024-04-03 23:32:37 -0300

    atualizações do sistema

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
-import FormCliente from './views/cliente/FormCliente';
-import ListCliente from './views/cliente/ListCliente';
-import FormEntregador from './views/entregador/FormEntregador';
-import ListEntregador from './views/entregador/ListEntregador';
-import Home from './views/home/Home';
-import FormProduto from './views/produto/FormProduto';
-import ListProduto from './views/produto/ListProduto';
+import FormCliente from './view/Cliente/FormCliente';
+import ListCliente from './view/Cliente/ListCliente';
+import FormEntregador from './view/Entregador/FormEntregador';
+import ListEntregador from './view/Entregador/ListEntregador';
+import Home from './view/Home/Home';
+import FormProduto from './view/Produto/FormProduto';
+import ListProduto from './view/Produto/ListProduto';
 
 function Rotas() {
     return (
